2e3ae4e51fae2614827424107220563a5d88e7bb
shao boran <shao_holy@hotmail.com>
2018-12-19 19:57:10 -0800

    20181219_Version

---
+++ backend/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/apple/Documents/Coding/React_Django/djreactv3/backend/env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
